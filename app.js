e5b0dd6670779e4391db9b64375fe4993d476894
Scottie Schneider <scott@laclave.co>
2017-09-04 22:00:18 -0500

    port back to 3000:
    :wq

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(80, function () {
+app.listen(3000, function () {
   console.log('Express app listening on port 3000');
 });
