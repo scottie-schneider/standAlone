43a1ef5eab85e4181ec47be3bc1aab1855ca505c
Scottie Schneider <scott@laclave.co>
2017-09-04 21:56:21 -0500

    changed port

--- app.js
+++ app.js
@@ -65,6 +65,6 @@ app.use(function(err, req, res, next) {
 });
 
 // listen on port 3000
-app.listen(3000, function () {
+app.listen(80, function () {
   console.log('Express app listening on port 3000');
 });
